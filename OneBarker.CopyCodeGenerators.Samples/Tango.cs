810276c049ac2e5a92db64f9676338a181b78d7d
Beau Barker <bbarker@karasent.com>
2026-05-15 12:01:37 -0400

    Added future consideration in Tango

---
+++ OneBarker.CopyCodeGenerators.Samples/Tango.cs
@@ -0,0 +1,15 @@
+﻿using Generators;
+
+namespace OneBarker.CopyCodeGenerators.Samples;
+
+[EnableUpdateExternal(typeof(System.Numerics.Vector2), typeof(System.Numerics.Vector3))]
+public partial class Tango
+{
+    // NOT SUPPORTED!
+    float Get_Z()
+        => 123.4f;
+
+    // CONSIDER SUPPORTING!
+    float Get_Z(System.Numerics.Vector2 source)
+        => source.X + source.Y;
+}
